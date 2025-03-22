f316acb51d456f3b2429997b0b8b25d3979b1f5e
OluwaseunSoft <Oluwaseunazure@yahoo.com>
2025-03-22 09:49:37 +0100

    master-branch-fix

--- Program.cs
+++ Program.cs
@@ -18,20 +18,34 @@
 #endregion
 
 #region InMemoryDatabase
-using LeetCodeHackerRankAlgorithms.InMemoryDatabase;
+//using LeetCodeHackerRankAlgorithms.InMemoryDatabase;
 
-InMemoryDb memoryDb = new InMemoryDb();
+//InMemoryDb memoryDb = new InMemoryDb();
 
-string[][] queries = [
-  ["SET_OR_INC", "A", "B", "5"],
-  ["SET_OR_INC", "A", "B", "6"],
-  ["GET", "A", "B"],
-  ["GET", "A", "C"],
-  ["DELETE", "A", "B"],
-  ["DELETE", "A", "C"]
-];
+//string[][] queries = [
+//  ["SET_OR_INC", "A", "B", "5"],
+//  ["SET_OR_INC", "A", "B", "6"],
+//  ["GET", "A", "B"],
+//  ["GET", "A", "C"],
+//  ["DELETE", "A", "B"],
+//  ["DELETE", "A", "C"]
+//];
+#endregion
 
-SlidingWindow slidingWindow = new SlidingWindow();
-Console.WriteLine("{0}", slidingWindow.LongestSubString("pwwkew"));
-Console.ReadLine();
+#region BFS
+using LeetCodeHackerRankAlgorithms.BFS;
 
+List<int>[] adj = new List<int>[5];
+adj[0] = new List<int> { 2, 3, 1 };
+adj[1] = new List<int> { 0 };
+adj[2] = new List<int> { 0, 4 };
+adj[3] = new List<int> { 0 };
+adj[4] = new List<int> { 2 };
+
+int src = 0;
+List<int> ans = BreadthFirstSearch.BFSOfGraph(adj, src);
+foreach (int a in ans)
+{
+    Console.Write(a + " ");
+}
+#endregion
